0d0c5eb8c50df3c79620d3f723cbf55b1663cae5
Edward Silverton <e.silverton@gmail.com>
2021-10-12 13:40:06 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+//const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.html$/i,
+  //       loader: "html-loader"
+  //     },
+  //     {
+  //       test: /\.png$/i,
+  //       loader: "url-loader"
+  //     }
+  //   ]
+  // },
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: "./src/index.html"
+  //   })
+  // ]
+};
